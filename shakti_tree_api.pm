1eaafe30b0d04f0729a9ef2cda47daa4581e16c1
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-03-26 00:18:18 +0530

    Export delete_node()

--- shakti_tree_api.pm
+++ shakti_tree_api.pm
@@ -6,7 +6,8 @@ our @EXPORT = qw(
         get_paracount get_para get_sentcount
         get_sent get_leaves get_fields
         modify_field get_field printstory
-        get_children get_nodes get_leaves_child);
+        get_children get_nodes get_leaves_child
+        delete_node);
 
 #			P.NIRUPAM PRATAP REDDY
 #				   UG3
